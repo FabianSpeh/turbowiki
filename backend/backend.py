c0e823c35c535e4d430be62ca1e042de0d13f2e0
Fabian Speh <132446305+FabianSpeh@users.noreply.github.com>
2025-10-20 14:57:46 +0200

    added basic flask endpoint, which accesses wikipedia api and allows to retrieve a pages content. added fetch from react frontend, which displays the content in correct styling

--- backend/backend.py
+++ backend/backend.py
@@ -1,8 +1,34 @@
 import time
 from flask import Flask
+from flask_cors import CORS
+import requests
+
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/time')
-def get_current_time():
-    return {'time':time.time()}
+@app.route('/article/<title>')
+def get_current_time(title):
+    headers = {
+    "User-Agent": "Turbowiki-MVP/0.1 (https://yourdomain.com; contact@example.com)"
+}
+    request = requests.get(f"https://en.wikipedia.org/w/api.php", params={
+        "action": "parse",
+        "page": title,
+        "prop": "text",
+        "format": "json",
+    
+    },
+    headers = headers)
+    data = request.json()
+    print(data)
+    
+    # response = {
+    #     "title": request.text["parse"]["title"],
+    #     "html": request.text["parse"]["text"]["*"],
+    # }
+    return data["parse"]["text"]["*"]
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+async function getWikipediaContent(title) {
+  const baseString = "http://localhost:5000/article/"
+  const finalString = baseString+title
+  const response = await fetch(finalString)
+  const html = await response.text()
+  return html
+}
+
 function App() {
   const [count, setCount] = useState(0)
+  const [articleHtml, setArticleHtml] = useState("")
 
   return (
     <>
@@ -16,18 +25,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <div className= "wiki-content" dangerouslySetInnerHTML={{__html: articleHtml}}></div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => getWikipediaContent("Albert_Einstein").then(html => setArticleHtml(html))}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
