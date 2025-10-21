daeacd2bc53d4c34cb6828a5dc9b8fbba4b5ef0e
Fabian Speh <132446305+FabianSpeh@users.noreply.github.com>
2025-10-21 07:01:14 +0200

    added functionality to be able navigate via hyperlinks on the pages. additonally added win condition which triggers, when you are on the same article as the goal article

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,68 @@ import Article from './Article'
 function App() {
   const [count, setCount] = useState(0)
   const [searchTerm, setSearchTerm] = useState("")
-  const [articleTitle, setArticleTitle] = useState("")
+  const [startingArticleTitle, setStartingArticleTitle] = useState("")
+  const [goalArticleTitle, setGoalArticleTitle] = useState("")
+  const [gameWon, setGameWon] = useState(false)
   
-  async function getRandomArticle(){
-    const randomArticle = await fetch("http://localhost:5000/article/random")
-    const title = await randomArticle.text()
-    setArticleTitle(title)
+//   async function getRandomStartingArticle(){
+//     const randomArticle = await fetch("http://localhost:5000/article/random")
+//     const title = await randomArticle.text()
+//     setStartingArticleTitle(title)
+    
+// }
+// async function getRandomGoalArticle(){
+//     const randomArticle = await fetch("http://localhost:5000/article/random")
+//     const title = await randomArticle.text()
+//     setGoalArticleTitle(title)
+    
+// }
+
+async function getTwoRandomArticles(){
+  const randomArticle1 = await fetch("http://localhost:5000/article/random")
+    const title1 = await randomArticle1.text()
+    setStartingArticleTitle(title1)
+
+  const randomArticle2 = await fetch("http://localhost:5000/article/random")
+    const title2 = await randomArticle2.text()
+    setGoalArticleTitle(title2)
     
 }
 
+function formatStrings(title){
+  return title.replaceAll("_", " ")
+}
+
+
+// every time the article changes, it checks for equality to see if the player has won
+useEffect(() => {
+  if (startingArticleTitle && startingArticleTitle === goalArticleTitle) {
+    setGameWon(true);
+  }
+}, [startingArticleTitle, goalArticleTitle]);
+
+// as soon as the player has won a message is logged
+useEffect(() => {
+  if (gameWon) {
+    console.log("YOU WON!!!!");
+    alert("🎉 YOU WON!!!! 🎉");
+  }
+}, [gameWon]);
   return (
     <>
     
       
-      <h1>{articleTitle}</h1>
-      <Article title ={articleTitle}></Article>
+      <h1>{formatStrings(startingArticleTitle)} → {formatStrings(goalArticleTitle)}</h1>
+      <div className='sidebar'></div>
+      <Article className="wiki-container"title ={startingArticleTitle} onNavigate={setStartingArticleTitle}></Article>
       <div className="card">
         
         <input name="search" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}></input>
-        <button onClick={getRandomArticle}></button>
+        <button onClick={getTwoRandomArticles}></button>
       </div>
       
     </>
   )
-}
 
+}
 export default App
--- src/Article.jsx
+++ src/Article.jsx
@@ -1,6 +1,7 @@
 import { useState,useEffect } from "react"
+import './Article.css'
 
-function Article({title}) {
+function Article({title, onNavigate}) {
 
 
 async function getWikipediaContent(title) {
@@ -10,6 +11,7 @@ async function getWikipediaContent(title) {
   const html = await response.text()
   return html
 }
+
 
 
 const [articleHtml, setArticleHtml] = useState("")
@@ -23,7 +25,33 @@ const [isLoading, setIsLoading] = useState(false)
       .finally(() => setIsLoading(false))
   }, [title])
 
+useEffect(() => {
+    // selects whole wiki content container
+    const container = document.querySelector(".wiki-content");
+    if(!container) return;
 
+    // handles the clicking on a link
+    //only need to attach a single event listener, since the event "bubbles up" to the parent container
+    function handleClick(event) {
+        const link = event.target.closest("a")
+        if(!link) return;
+
+        // finds actual link and changes the url so it doesn't redirect to actual wikipedia
+        const href = link.getAttribute("href");
+        if (href && href.startsWith("/wiki/")){
+            event.preventDefault();
+            const newTitle = decodeURIComponent(href.replace("/wiki/",""))
+            onNavigate(newTitle)
+            checkWin()
+
+        }
+
+    }
+    container.addEventListener("click",handleClick);
+    return () => container.removeEventListener("click", handleClick);
+   
+
+}, [articleHtml])
 if (isLoading) return <p>Loading Article..</p>
 return <div className="wiki-content" dangerouslySetInnerHTML={{__html: articleHtml}}/>
 }
